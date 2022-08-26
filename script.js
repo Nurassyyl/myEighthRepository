7619f38f807d63fbf587a9c137f413405ae95577
Nurassyl Abdildabek <abdildabekn@mail.ru>
2022-08-26 17:32:54 +0600

    Выполнил задание 16-го

---
+++ script.js
@@ -0,0 +1,17 @@
+class First {
+    hello() {
+        console.log("Привет я метод родителя!");
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
+
+// const first = new First();
+// first.hello();
+const second = new Second();
+second.hello();
